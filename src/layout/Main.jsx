9453ea5a17a6637e9268f027751627c9e5b91c65
skSadi12 <sksadi433@gmail.com>
2026-07-02 15:18:42 +0600

    photos added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 // import Footer from "../components/Footer";
 // import Navbar from "../Components/Navbar";
 
+
 const Main = () => {
   return (
     <div className="font-sans antialiased  tetx-black  ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      <p className='text-red-600 font-bold'>test</p>
+    <div >
+      
     </div>
   )
 }
