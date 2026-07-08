6fa734877afa4511b585e8d4bce7edbb70bf88ab
skSadi12 <sksadi433@gmail.com>
2026-07-08 16:02:28 +0600

    cart ui added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.svg"
 import ring from "../assets/ring.svg"
 import moon from "../assets/icons/moon.svg"
 import shopping from "../assets/shopping-cart.svg"
+import Cart from "../cine/Cart";
 
 const Header = () => {
+  const[showCart, setShowCart] = useState(false)
+  function onHandleCartShow(){
+    setShowCart(true)
+  }
+  function handleHideCart(){
+    setShowCart(false)
+  }
   return (
     <header>
+      {showCart && <Cart  onCancel={handleHideCart}/>}
+      
       <nav className="container mx-auto flex items-center justify-between space-x-10 py-6">
         {/* Logo */}
         <a href="/">
@@ -50,6 +60,7 @@ const Header = () => {
 
           <li>
             <a
+              onClick={onHandleCartShow}
               href="#"
               className="inline-block rounded-lg bg-primary/20 p-1 backdrop-blur-[2px] dark:bg-primary/[7%]"
             >
